28f392b811062e46a2efaad150dfe9f96bea6d26
Sankalp Vyas <sankalpvyas.2011@gmail.com>
2022-01-01 20:55:37 +0530

    Version 1

--- OddEven.py
+++ OddEven.py
@@ -1,7 +1,7 @@
 #python program for determining odd and even Numbers
 while 1==1: #to make our program run forever
     try:
-        n=int((input("Enter Your Number: "))) #odd and even is property of integers only
+        n=int((input("Enter Your Number: ")))
     except:
         print("Only integers allowed")
         exit()
